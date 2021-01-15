e248d1cb0fb8f36aafda66bd37b948a8819ec73e
Pratyush M <mpratyush2008@gmail.com>
2021-01-15 06:33:35 +0530

    Updated Program 8

--- prog8b.php
+++ prog8b.php
@@ -55,10 +55,10 @@ if($n == $p)
         for($j = 0; $j < $q; $j++)
         {
             $result[$i][$j] = 0;
-        }
-        for($k = 0; $k < $n; $k++)
-        {
-            $result[$i][$j] += $a[$i][$k] * $b[$k][$j];
+            for($k = 0; $k < $n; $k++)
+            {
+                $result[$i][$j] += $a[$i][$k] * $b[$k][$j];
+            }
         }
     }
     for($row = 0; $row < $m; $row++)
